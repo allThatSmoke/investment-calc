797d979ac52c87bea097fe708fb943821a333a62
Govind Dandekar <govind@Govinds-Mac-mini.local>
2024-08-12 20:51:10 -0700

    Add UserInput with state

--- src/components/UserInput.jsx
+++ src/components/UserInput.jsx
@@ -1,31 +1,73 @@
+import { useState } from 'react';
+
+const INITIAL_PARAMS = {
+	initialInvestment: 10000,
+	annualInvestment: 1200,
+	expectedReturn: 6,
+	duration: 10
+}
+
 export default function UserInput(){
-	
-	
-	
-	
+	const [investmentParams, setInvestmentParams] = useState(INITIAL_PARAMS)
+
+	function handleChange(inputIdentifier, newValue){
+		setInvestmentParams((prevParams) => {
+			return {
+				...prevParams,
+				[inputIdentifier]: newValue
+			}
+		})
+	}
+
 	return (
+		<>
 		<section id="user-input" >
 			<div className="input-group">
 			    <p>
 					<label>Initial Investment</label>
-					<input type="number" required></input>
+					<input 
+					  type="number" 
+					  required
+					  value={investmentParams.initialInvestment}
+					  onChange={(event)=>handleChange("initialInvestment", event.target.value)}>
+					</input>
 				</p>
 				<p>
 					<label>Annual Investment</label>
-					<input type="number" required></input>
+					<input 
+					  type="number" 
+					  required
+					  value={investmentParams.annualInvestment}
+					  onChange={(event)=>handleChange("annualInvestment", event.target.value)}>
+					</input>
 				</p>
 			</div>
 			<p></p>
 			<div className="input-group">
 				<p>
 					<label>Expected Return</label>
-					<input type="number" required></input>
+					<input 
+					  type="number"
+					  required
+					  value={investmentParams.expectedReturn}
+					  onChange={(event)=>handleChange("expectedReturn", event.target.value)}></input>
 				</p>
 				<p>
 					<label>Duration</label>
-					<input type="number" required></input>
+					<input 
+					  type="number" 
+					  required
+					  value={investmentParams.duration}
+					  onChange={(event)=>handleChange("duration", event.target.value)}></input>
 				</p>
 			</div>
 		</section>
+		<section>
+			<p>Initial Investment Value: {investmentParams.initialInvestment}</p>
+			<p>Annual Investment Value: {investmentParams.annualInvestment}</p>
+			<p>Expected Return: {investmentParams.expectedReturn}</p>
+			<p>Duration: {investmentParams.duration}</p>
+		</section>
+		</>
 	)
 }
